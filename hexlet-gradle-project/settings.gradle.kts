1802bd44a7d808f0737ec3f5e914958dfaabe0c7
LiliiaDalul <97311050+LiliiaDalul@users.noreply.github.com>
2024-01-01 22:13:53 +0300

    Update settings.gradle.kts

--- hexlet-gradle-project/settings.gradle.kts
+++ hexlet-gradle-project/settings.gradle.kts
@@ -7,3 +7,18 @@
  */
 
 rootProject.name = "hexlet-gradle-project"
+plugins {
+    // Поддержка запуска из командной строки
+    application
+}
+
+repositories {
+    // Подключаем автоматическая работа с репозиторием Maven Central
+    mavenCentral()
+}
+
+// Для плагина application указываем главный класс приложения
+application {
+    // Входная точка
+    mainClass.set("io.hexlet.example.App")
+}
