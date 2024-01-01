1fa389a5b4d39dc0e876e1422908ce7c5178d5e5
LiliiaDalul <97311050+LiliiaDalul@users.noreply.github.com>
2024-01-01 21:44:36 +0300

    Update Example.java

--- io/hexlet/build/Example.java
+++ io/hexlet/build/Example.java
@@ -5,6 +5,6 @@ import org.apache.commons.lang3.StringUtils;
 public class Example {
     public static void main(String[] args) {
         var capitalizedWord = StringUtils.capitalize("hexlet");
-        System.out.println(StringBuilder.reverse(capitalizedWord));
+        System.out.println(StringUtils.reverse(capitalizedWord));
     }
 }
